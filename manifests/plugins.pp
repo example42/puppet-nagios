fe86baa7f75c280447e506895cc228c7d0227916
Dan Schaefer <dan@schaeferzone.net>
2013-06-12 15:54:07 -0400

    Adding a template variable for the check_disks plugin

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -8,11 +8,16 @@
 #
 class nagios::plugins {
 
+  $nagios_check_disks_nrpe_cfg = $::nagios_check_disks_nrpe_cfg ? {
+    ''      => 'nagios/nrpe_cfg/nrpe-check_disk.cfg.erb',
+    default => $::nagios_check_disks_nrpe_cfg,
+  }
+
   # Include Extra custom Plugins (Provided via Puppet)
   nagios::plugin { 'check_mount': } 
   nagios::plugin { 'check_disks':
     source   => 'no' ,
-    nrpe_cfg => 'nagios/nrpe_cfg/nrpe-check_disk.cfg.erb',
+    nrpe_cfg => $nagios_check_disks_nrpe_cfg,
   }
   nagios::plugin { 'check_yum':
     nrpe_cfg => 'nagios/nrpe_cfg/nrpe-check_yum.cfg.erb',
